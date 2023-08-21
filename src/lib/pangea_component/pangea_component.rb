70d3ebc6228db68a8715e5f47f355c2acb6dd122
luis <ldesiqueira@pinger.com>
2023-08-21 11:46:29 -0300

    track vpc

--- src/lib/pangea_component/pangea_component.rb
+++ src/lib/pangea_component/pangea_component.rb
@@ -4,6 +4,14 @@ def symbolize(hash)
   JSON[JSON[hash], symbolize_names: true]
 end
 
+def deep_open_struct(hash)
+  Struct.new(
+    hash.map do |key, value|
+      [key, value.is_a?(Hash) ? deep_open_struct(value) : value]
+    end
+  ).tap.call(&:freeze)
+end
+
 def component(**kwargs)
   kwargs        = symbolize(kwargs) unless kwargs.nil?
   resource_name = kwargs.fetch(:resource, nil)
