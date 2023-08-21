d1b71e863c6b676e9800957a414b7e39179f57e3
luis <ldesiqueira@pinger.com>
2023-08-21 09:52:01 -0300

    sync

--- spec/component_spec.rb
+++ spec/component_spec.rb
@@ -5,14 +5,14 @@ describe %(component interface) do
   TF = TerraformSynthesizer.new
   it %(returns a Hash) do
     TF.synthesize do
-      results = component(
-        resource: :aws_vpc, 
-        name: %(test_vpc),
-        properties: {
-          cidr_block: %(10.0.0.0/16),
-          tags: Name: %(test_vpc)
-        }
-      )
+      # results = component(
+      #   resource: :aws_vpc,
+      #   name: %(test_vpc),
+      #   properties: {
+      #     cidr_block: %(10.0.0.0/16),
+      #     tags: Name: %(test_vpc)
+      #   }
+      # )
       expect(component).to be_kind_of(Hash)
     end
   end
