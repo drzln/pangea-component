4ab00c9e1e03c7d08a0c78065554cfd6fd28a190
luis <ldesiqueira@pinger.com>
2023-08-21 10:16:55 -0300

    sync

--- src/lib/pangea_component/pangea_component.rb
+++ src/lib/pangea_component/pangea_component.rb
@@ -5,22 +5,11 @@ def symbolize(hash)
 end
 
 def component(**kwargs)
-  # ensure everything passed into component
-  # is consistently keyed by symbols
-  kwargs = symbolize(kwargs) unless kwargs.nil?
-
-  # perform any sort of validation on passed in
-  # component data
-  # raise "args support not implemented yet" if args.any?
-  # raise "kwargs must be present" unless kwargs.any?
-  # raise "component name not specified" unless kwargs[:name]
-  # raise "#{kwargs[:name]} has no properties" unless kwargs[:properties]
-
+  kwargs        = symbolize(kwargs) unless kwargs.nil?
   resource_name = kwargs.fetch(:resource, nil)
   virtual_name  = kwargs.fetch(:name, nil)
   properties    = kwargs.fetch(:properties, {})
 
-  # resource call which triggers compilation
   resource(resource_name, virtual_name) do
     properties.each_key do |method|
       args = kwargs.dig(:properties, pn)
